86db2bbf16e1a6d1ff3684439d35357b931e0c20
Alex Shulgin <alex.shulgin@gmail.com>
2020-12-05 20:42:23 +0100

    Extend projection matrix far cut-off limit to 200

--- WebGL_Cube_files/webgl-demo.js
+++ WebGL_Cube_files/webgl-demo.js
@@ -679,7 +679,7 @@ function drawScene(gl, programInfo, buffers, deltaTime) {
   const fieldOfView = rad(60);
   const aspect = gl.canvas.clientWidth / gl.canvas.clientHeight;
   const zNear = 0.1;
-  const zFar = 100.0;
+  const zFar = 200.0;
   const projectionMatrix = mat4.create();
 
   // note: glmatrix.js always has the first argument
